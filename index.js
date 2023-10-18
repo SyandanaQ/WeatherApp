2980f3d3224a8be60761f11445d926e58a7e69f9
SyandanaQ <syandana195@gmail.com>
2023-10-18 14:28:55 +0700

    updated index

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const error404 = document.querySelector('.not-found');
 
 search.addEventListener('click', () => {
 
-    const APIKey = '';
+    const APIKey = 'CODE API';
     const city = document.querySelector('.search-box input').value;
 
     if (city === '')
